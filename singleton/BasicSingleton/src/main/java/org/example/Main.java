6b1a66231f5f07145fed8045f6f5ff5d6be7216b
ITAKEB <tomascf123@gmail.com>
2023-08-19 19:56:53 -0500

    Add some fixes for serialization

--- singleton/BasicSingleton/src/main/java/org/example/Main.java
+++ singleton/BasicSingleton/src/main/java/org/example/Main.java
@@ -1,12 +1,15 @@
 package org.example;
 
-class BasicSingleton {
+import java.io.*;
+
+class BasicSingleton implements Serializable {
     private BasicSingleton() {
 
     }
+
     private static final BasicSingleton INSTANCE = new BasicSingleton();
 
-    public static BasicSingleton getInstance(){
+    public static BasicSingleton getInstance() {
         return INSTANCE;
     }
 
@@ -19,13 +22,45 @@ class BasicSingleton {
     public void setValue(int value) {
         this.value = value;
     }
+
+    protected Object readResolve(){
+        return INSTANCE;
+    }
 }
 
 public class Main {
-    public static void main(String[] args) {
-        // IntelliJ IDEA suggests fixing it.
+
+    static void saveToFile(BasicSingleton singleton, String fileName) throws Exception {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
+            objectOutputStream.write(singleton.getValue());
+        }
+    }
+
+    static BasicSingleton readFromFile(String filename) throws Exception {
+        try (FileInputStream fileIn = new FileInputStream(filename);
+             ObjectInputStream in = new ObjectInputStream(fileIn)) {
+            return (BasicSingleton) in.readObject();
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        //Problems
+        //1. reflection
+        //2. serialization
+
         BasicSingleton singleton = BasicSingleton.getInstance();
-        singleton.setValue(123);
+        singleton.setValue(111);
+
+        String fileName = "singleton.bin";
+        saveToFile(singleton, fileName);
+        singleton.setValue(222);
+
+        BasicSingleton singleton2 = readFromFile(fileName);
+
+        System.out.println(singleton2 == singleton);
+
         System.out.println(singleton.getValue());
+        System.out.println(singleton2.getValue());
     }
 }
